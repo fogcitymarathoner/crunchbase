c822c802d83a69971df1c899c617d4d75d47c265
marc condon <marc.f.condon@hp.com>
2015-04-15 15:49:00 -0700

    Cleaned up the timers in get companies index.

--- scripts/get_companies.py
+++ scripts/get_companies.py
@@ -1,13 +1,17 @@
 import requests
 import pymongo
 import json
-import sys
-import sched, time
 
-def strip_non_ascii(string):
-    ''' Returns the string without non ASCII characters'''
-    stripped = (c for c in string if 0 < ord(c) < 127)
-    return ''.join(stripped)
+import daemon # install python-daemon from pypi
+import os
+import sys
+import sched
+import time
+from datetime import datetime as dt
+import datetime
+import logging
+
+
 
 def get_page_record_page(x, db):
     """
@@ -17,10 +21,9 @@ def get_page_record_page(x, db):
     :return:
     """
     r = requests.get('https://api.crunchbase.com/v/2/organizations?user_key=4c8d0795c93056f45eb38d1a16ddd71f&page=%s'%x)
-    #print strip_non_ascii(r.text)
+
     data = json.loads(r.text)
-    #print r.text
-    #quit()
+
     for y in data['data']['items']:
         o = {
           'name': y['name'],
@@ -42,14 +45,6 @@ def get_page_record_page(x, db):
         scheduler.enterabs(time.mktime(t.timetuple()), 1, get_page_record_page, (x+1, db))
         scheduler.run()
 
-import daemon # install python-daemon from pypi
-import os
-import sys
-import sched
-import time
-from datetime import datetime as dt
-import datetime
-import logging
 
 def now_str():
     """Return hh:mm:ss string representation of the current time."""
@@ -58,43 +53,9 @@ def now_str():
 
 
 def main():
-    def do_something_again(message):
-        print 'RUNNING:', now_str(), message
-        logging.debug('RUNNING:', now_str(), message)
 
-
-        client = pymongo.MongoClient()
-        db = client.crunchbase
-        os.system('echo hi')
-        # Do whatever you need to do here
-        # then re-register task for same time tomorrow
-        # Put task for today at 7am on queue. Executes immediately if past 7am
-        """
-        new_date = dt.now() + datetime.timedelta(minutes=5)
-        daily_time = new_date.time()
-        print daily_time
-        #quit()
-        t = dt.combine(dt.now() + datetime.timedelta(days=0), daily_time)
-        logging.debug('RUNNING: again')
-        scheduler = sched.scheduler(time.time, time.sleep)
-        scheduler.enterabs(time.mktime(t.timetuple()), 1, do_something_again, ('Running again',))
-        scheduler.run()
-        """
-
-        #i = 1
-        #for x in range(0, 300):
-        print time.time()
-        new_date = dt.now() + datetime.timedelta(minutes=5)
-        daily_time = new_date.time()
-        print daily_time
-        #quit()
-        t = dt.combine(dt.now() + datetime.timedelta(days=0), daily_time)
-        logging.debug('RUNNING: again')
-        scheduler = sched.scheduler(time.time, time.sleep)
-        scheduler.enterabs(time.mktime(t.timetuple()), 1, get_page_record_page, (1, db))
-        scheduler.run()
-
-
+    client = pymongo.MongoClient()
+    db = client.crunchbase
 
     # Build a scheduler object that will look at absolute times
     scheduler = sched.scheduler(time.time, time.sleep)
@@ -103,9 +64,8 @@ def main():
     daily_time = datetime.time(8)
     first_time = dt.combine(dt.now(), daily_time)
     # time, priority, callable, *args
-    logging.debug('RUN the first time')
-    scheduler.enterabs(time.mktime(first_time.timetuple()), 1,
-                       do_something_again, ('Run the first time',))
+
+    scheduler.enterabs(time.mktime(t.timetuple()), 1, get_page_record_page, (1, db))
     scheduler.run()
 
 if __name__ == '__main__':
@@ -114,25 +74,3 @@ if __name__ == '__main__':
     else:
         with daemon.DaemonContext():
             main()
-
-"""
-
-def main():
-
-    client = pymongo.MongoClient()
-    db = client.crunchbase
-    s = sched.scheduler(time.time, time.sleep)
-    i = 1
-    for x in range(0, 300):
-        print time.time()
-        s.enter(5*i, 1, b))get_page_record_page, (x, d
-        s.run()
-        i += 1
-        print time.time()
-
-    print 'done!'
-
-if __name__ == "__main__":
-    sys.exit(main())
-
-"""
